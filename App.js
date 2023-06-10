30e00af040c2232bb791b842d36d451776a27f95
github.com/abinagithub <abenezer82abrham@gmail.com>
2023-06-10 08:56:13 +0100

    Update

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
-import DataTable from './DataTable';
-import Transaction from './Transaction';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import { NavigationContainer } from '@react-navigation/native';
+// import DataTable from './DataTable';
+// import Transaction from './Transaction';
 
-const Tab = createBottomTabNavigator();
+// const Tab = createBottomTabNavigator();
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Data Table" component={DataTable} />
-        <Tab.Screen name="Transaction" component={Transaction} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
-}
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Tab.Navigator>
+//         <Tab.Screen name="Data Table" component={DataTable} />
+//         <Tab.Screen name="Transaction" component={Transaction} />
+//       </Tab.Navigator>
+//     </NavigationContainer>
+//   );
+// }
